eeed100cb3c9016d68258bcc86238e489a3cc992
WalkerKim <cuk25president@gmail.com>
2023-01-01 22:15:35 +0900

    추천 post 로직 구현, remove heading id list -> to html dom parsing, parsed md css edit, apply debounce to toc

--- management-front/src/routes/post/ContentProps.svelte
+++ management-front/src/routes/post/ContentProps.svelte
@@ -2,8 +2,10 @@
     export let content;
     let tagListString = Array.isArray(content.tagList) ? content.tagList.map(i=>i.id).join(",") : "";
     let ogKeywordListString = Array.isArray(content.ogKeywordList) ? content.ogKeywordList.join(",") : "";
+    let recommendPostIdList = Array.isArray(content.recommendPostIdList) ? content.recommendPostIdList.join(",") : "";
     $: content.tagList = stringToArray(tagListString);
     $: content.ogKeywordList = stringToArray(ogKeywordListString);
+    $: content.recommendPostIdList = stringToArray(recommendPostIdList);
 
     function stringToArray(inputString){
         return [... new Set(inputString.replaceAll(" ", "").split(",").filter(string=>string!=""))]
@@ -63,6 +65,17 @@
             </div>
             <div class="w-full">
                 <input type="text" name="ogKeywordList" id="ogKeywordList" bind:value={ogKeywordListString}
+                       class="col-span-1 border-black border w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            </div>
+        </div>
+        <hr>
+        <div class="col-span-1 py-1 my-2 flex">
+            <div class="w-36 justify-between flex">
+                <label for="ogKeywordList" class="self-center"> recommend Post Id List</label>
+                <label for="ogKeywordList" class="self-center px-3"> : </label>
+            </div>
+            <div class="w-full">
+                <input type="text" name="recommendPostIdList" id="recommendPostIdList" bind:value={recommendPostIdList}
                        class="col-span-1 border-black border w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
         </div>
