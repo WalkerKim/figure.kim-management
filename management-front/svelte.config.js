bb7a65ca1928c1f8d22b10eb87dc851565111110
WalkerKim <cuk25president@gmail.com>
2023-02-28 16:47:42 +0900

    changing svelte build policy

--- management-front/svelte.config.js
+++ management-front/svelte.config.js
@@ -2,21 +2,28 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 import 'dotenv/config';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '200.html',
-				pages: process.env.VITE_BUILD_PATH??'build',
-				assets: process.env.VITE_BUILD_PATH??'build',
-		},
-			)
-	},
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
+    kit: {
+        adapter: adapter({
+            // pages: 'build',
+            // assets: 'build',
+            fallback: '200.html',
+                // pages: process.env.VITE_BUILD_PATH ?? 'build',
+                // assets: process.env.VITE_BUILD_PATH ?? 'build',
+            // strict:true
+            }
+        ),
+        prerender:{
+            entries: []
+        }
+    },
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 };
 
 
